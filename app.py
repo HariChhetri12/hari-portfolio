599300f8c4c1fe64d0086c01dcfdc9f2f9345ec9
HariChhetri12 <harichhetri.hc12@gmail.com>
2025-04-27 22:23:40 +0600

    Update app.py

--- app.py
+++ app.py
@@ -99,17 +99,11 @@ Expected Graduation: 202*
 # --- Contact Section ---
 st.header("📬 Contact Me")
 
-st.markdown(
-    """
-    - 📧 Email: <a href="mailto:harichhetri.hc12@gmail.com" target="_blank">harichhetri.hc12@gmail.com</a>
-    - 📞 Phone: <a href="tel:17794981">17794981</a>
-    - 🏢 CEO at **H.C12 Company**
-    """,
-    unsafe_allow_html=True
-)
-
-
-
+st.markdown("""
+- 📧 **Email:** [harichhetri.hc12@gmail.com](mailto:harichhetri.hc12@gmail.com)
+- 📞 **Phone:** [17794981](tel:17794981)
+- 🏢 **CEO at H.C12 Company**
+""", unsafe_allow_html=True)
 
 # --- Footer ---
 st.write("---")
